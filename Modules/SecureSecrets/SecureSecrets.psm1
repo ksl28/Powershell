4ea18fca0639f772d100f7c0ce45e0a24062dcd2
Kristian Leth <krle@jysk.com>
2021-09-14 11:06:05 +0200

    Exported module members

--- Modules/SecureSecrets/SecureSecrets.psm1
+++ Modules/SecureSecrets/SecureSecrets.psm1
@@ -1,13 +1,17 @@
 
-    $Public = @(Get-ChildItem -Path $PSScriptRoot\public\*.ps1 -ErrorAction SilentlyContinue )
-    $Private = @(Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
-    foreach ($function in @($Public + $Private)) {
-        try {
-            Import-Module $function.FullName -ErrorAction stop
-        }
-        catch {
-            Write-Host "Failed to import $($function.fullname)" -ForegroundColor red
-            $_.Exception.Message
-            break
-        }
+$Public = @(Get-ChildItem -Path $PSScriptRoot\public\*.ps1 -ErrorAction SilentlyContinue )
+$Private = @(Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+foreach ($function in @($Public + $Private)) {
+    try {
+        Import-Module $function.FullName -ErrorAction stop
     }
+    catch {
+        Write-Host "Failed to import $($function.fullname)" -ForegroundColor red
+        $_.Exception.Message
+        break
+    }
+}
+## Export all of the public functions making them available to the user
+foreach ($file in $Public) {
+    Export-ModuleMember -Function $file.BaseName
+}
